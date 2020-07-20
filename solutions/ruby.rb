b4060d2fc694e834be49a3458ac1101fa216fc8b
misteroh <madeline@done-digital.com>
2020-07-20 14:29:28 -0700

    add solutions

---
+++ solutions/ruby.rb
@@ -0,0 +1,30 @@
+
+# Fizz buzz with loop
+# 1.upto(15) do |i|
+#     if i % 3 == 0 && i % 5 == 0 
+#         p "fizzbuzz"
+#     elsif i % 3 == 0
+#         p "fizz"
+#     elsif i % 5 == 0
+#         p "buzz"
+#     else
+#         p i
+#     end  
+# end
+
+# Fizz buzz with a method
+# def fizzbuzz num
+#     1.upto(num) do |i|
+#         if i % 3 == 0 && i % 5 == 0 
+#             p "fizzbuzz"
+#         elsif i % 3 == 0
+#             p "fizz"
+#         elsif i % 5 == 0
+#             p "buzz"
+#         else
+#             p i
+#         end  
+#     end
+# end
+
+# fizzbuzz 10
